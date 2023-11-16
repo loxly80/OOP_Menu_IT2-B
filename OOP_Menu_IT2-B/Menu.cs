1fd0cd9fa63a06f7743ca32ab10fb629cd0c398d
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-11-16 15:28:37 +0100

    Po hodině

---
+++ OOP_Menu_IT2-B/Menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Menu_IT2_B
+{
+  public class Menu
+  {
+    public string Nadpis = "";
+    public ConsoleColor BarvaOkraj = ConsoleColor.Green;
+    public ConsoleColor BarvaPozadi = ConsoleColor.Black;
+    public ConsoleColor BarvaText = ConsoleColor.White;
+    public ConsoleColor BarvaKurzor = ConsoleColor.Yellow;
+    public int Sirka = 25;
+    public string[] Polozky = new string[0];
+
+    public void Zobraz()
+    {
+      ZobrazOkraj();
+      ZobrazNadpis();
+      //ZobrazKurzor();
+      //ZobrazPolozky();
+    }
+
+    private void ZobrazNadpis()
+    {
+      Console.BackgroundColor = BarvaPozadi;
+      Console.ForegroundColor = BarvaText;
+      Console.SetCursorPosition(3, 1);
+      Console.Write(Nadpis);
+    }
+
+    private void ZobrazOkraj()
+    {
+      Console.BackgroundColor = BarvaPozadi;
+      Console.ForegroundColor = BarvaOkraj;
+      Console.Write("╔");      
+      Console.Write(new String('═',Sirka));
+      Console.WriteLine("╗");
+      Console.Write("║");     
+      Console.Write(new String(' ', Sirka));
+      Console.WriteLine("║");
+      Console.Write("╠");
+      Console.Write(new String('═', Sirka));
+      Console.WriteLine("╣");
+      for(int i = 0; i < Polozky.Length; i++)
+      {
+        Console.Write("║");
+        Console.Write(new String(' ', Sirka));
+        Console.WriteLine("║");
+      }
+      Console.Write("╚");
+      Console.Write(new String('═', Sirka));
+      Console.WriteLine("╝");
+    }
+  }
+}
--- OOP_Menu_IT2-B/Program.cs
+++ OOP_Menu_IT2-B/Program.cs
@@ -1,28 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.CursorVisible = false;
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("╔════════════════════╗");
-Console.WriteLine("║                    ║");
-Console.WriteLine("╠════════════════════╣");
-Console.WriteLine("║                    ║");
-Console.WriteLine("║                    ║");
-Console.WriteLine("║                    ║");
-Console.WriteLine("╚════════════════════╝");
-Console.ForegroundColor = ConsoleColor.White;
-Console.SetCursorPosition(6,1);
-Console.Write("SUPER MENU");
-Console.SetCursorPosition(6, 3);
-Console.Write("Položka 1");
-Console.BackgroundColor = ConsoleColor.Yellow;
-Console.ForegroundColor = ConsoleColor.Black;
-Console.SetCursorPosition(1, 4);
-Console.Write("                    ");
-Console.SetCursorPosition(6, 4);
-Console.Write("Položka 2");
-Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.White;
+//Console.CursorVisible = false;
+//Console.ForegroundColor = ConsoleColor.Green;
+//Console.WriteLine("╔════════════════════╗");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("╠════════════════════╣");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("║                    ║");
+//Console.WriteLine("╚════════════════════╝");
+//Console.ForegroundColor = ConsoleColor.White;
+//Console.SetCursorPosition(6,1);
+//Console.Write("SUPER MENU");
+//Console.SetCursorPosition(6, 3);
+//Console.Write("Položka 1");
+//Console.BackgroundColor = ConsoleColor.Yellow;
+//Console.ForegroundColor = ConsoleColor.Black;
+//Console.SetCursorPosition(1, 4);
+//Console.Write("                    ");
+//Console.SetCursorPosition(6, 4);
+//Console.Write("Položka 2");
+//Console.BackgroundColor = ConsoleColor.Black;
+//Console.ForegroundColor = ConsoleColor.White;
 
-Console.SetCursorPosition(6, 5);
-Console.Write("Položka 3");
+//Console.SetCursorPosition(6, 5);
+//Console.Write("Položka 3");
+
+using OOP_Menu_IT2_B;
+Console.CursorVisible = false;
+
+Menu menu = new Menu();
+menu.Nadpis = "Moje MENU";
+menu.Zobraz();
+menu.Polozky = new string[5];
+menu.Zobraz();
+
 Console.ReadKey();
